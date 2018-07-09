3659d523e8cfa1cec213c8a16b4a213501dcd99e
William Hendricks <wshendricks@gmail.comgit config --global user.email wshendricks@gmail.comgit config --global user.email wshendricks@gmail.com>
2018-07-09 09:47:00 -0500

    Next step is Sidenav.

---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+} from '@angular/material';
+
+
+
+@NgModule({
+    imports: [],
+    exports: []
+})
+export class MaterialModule {}
+
+// From the tutorial: The idea of creating a new Angular module (@NgModule) is
+// to centralize what you will import from Angular Material in a single file.
